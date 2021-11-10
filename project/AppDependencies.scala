9ec4cc71e16d593fe2f877446eecfa99ada7e202
pratikrana06 <63359242+pratikrana06@users.noreply.github.com>
2021-11-10 11:00:09 +0000

    DEP-2275 : Library Upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 object AppDependencies {
 
   lazy val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "play-partials" % "8.1.0-play-28",
-    "uk.gov.hmrc" %% "bootstrap-common-play-28" % "5.0.0"
+    "uk.gov.hmrc" %% "play-partials"            % "8.2.0-play-28",
+    "uk.gov.hmrc" %% "bootstrap-common-play-28" % "5.16.0"
   )
 
   lazy val test: Seq[ModuleID] = Seq(
-    "com.typesafe.play" %% "play-test" % "2.8.7" % "test",
-    "org.pegdown"    % "pegdown"      % "1.6.0"  % "test",
-    "org.scalatest" %% "scalatest"    % "3.0.8"  % "test",
-    "org.mockito"    % "mockito-core"  % "3.5.13" % "test"
+    "com.typesafe.play" %% "play-test"    % "2.8.8"  % "test",
+    "org.pegdown"       %  "pegdown"      % "1.6.0"  % "test",
+    "org.scalatest"     %% "scalatest"    % "3.0.9"  % "test",
+    "org.mockito"       %  "mockito-core" % "4.0.0"  % "test"
   )
 }
