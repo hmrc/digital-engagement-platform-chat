54e97c9849f0c275a000acffd3ae981fe7f6e0b2
pratikrana06 <63359242+pratikrana06@users.noreply.github.com>
2022-05-24 10:33:51 +0100

    [DEP-3048][PR]:Library upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,14 @@ object AppDependencies {
 
   lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "play-partials"            % "8.3.0-play-28",
-    "uk.gov.hmrc" %% "bootstrap-common-play-28" % "5.21.0"
+    "uk.gov.hmrc" %% "bootstrap-common-play-28" % "5.24.0"
   )
 
   lazy val test: Seq[ModuleID] = Seq(
-    "com.typesafe.play"     %% "play-test"    % "2.8.14"  % "test",
+    "com.typesafe.play"     %% "play-test"    % "2.8.15"  % "test",
     "org.pegdown"           %  "pegdown"      % "1.6.0"   % "test",
-    "org.scalatest"         %% "scalatest"    % "3.2.11"  % "test",
-    "org.mockito"           %  "mockito-core" % "4.4.0"   % "test",
+    "org.scalatest"         %% "scalatest"    % "3.2.12"  % "test",
+    "org.mockito"           %  "mockito-core" % "4.5.1"   % "test",
     "com.vladsch.flexmark"  % "flexmark-all"  % "0.62.2"  % "test"
   )
 }
