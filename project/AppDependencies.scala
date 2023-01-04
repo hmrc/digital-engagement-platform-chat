23e3d4428416ebc8f53fa88de1b0710495675d97
Tony Myers <tonymyers@hotmail.com>
2023-01-04 13:56:07 +0000

    DEP-3722: Update to Scala 2.13.10

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,11 +4,11 @@ object AppDependencies {
 
   lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "play-partials"            % "8.3.0-play-28",
-    "uk.gov.hmrc" %% "bootstrap-common-play-28" % "6.3.0"
+    "uk.gov.hmrc" %% "bootstrap-common-play-28" % "7.12.0"
   )
 
   lazy val test: Seq[ModuleID] = Seq(
-    "com.typesafe.play"     %% "play-test"    % "2.8.15"  % "test",
+    "com.typesafe.play"     %% "play-test"    % "2.8.18"  % "test",
     "org.pegdown"           %  "pegdown"      % "1.6.0"   % "test",
     "org.scalatest"         %% "scalatest"    % "3.2.12"  % "test",
     "org.mockito"           %  "mockito-core" % "4.5.1"   % "test",
