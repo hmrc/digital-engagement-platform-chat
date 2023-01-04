97940b7afaed3f7022ee6de51d198d14183a05dd
Tony Myers <tonymyers@hotmail.com>
2023-01-04 17:27:11 +0000

    DEP-3722: Update updated dependinces

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
   lazy val test: Seq[ModuleID] = Seq(
     "com.typesafe.play"     %% "play-test"    % "2.8.18"  % "test",
     "org.pegdown"           %  "pegdown"      % "1.6.0"   % "test",
-    "org.scalatest"         %% "scalatest"    % "3.2.12"  % "test",
+    "org.scalatest"         %% "scalatest"    % "3.2.14"  % "test",
     "org.mockito"           %  "mockito-core" % "4.5.1"   % "test",
     "com.vladsch.flexmark"  % "flexmark-all"  % "0.62.2"  % "test"
   )
