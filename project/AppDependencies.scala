b8e6b1b39b7ce063358976bde58d46a40264a349
Michael Sweeton <mikesweeton@gmail.com>
2023-05-26 11:02:27 +0100

    DEP-4452 pr bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,10 +8,11 @@ object AppDependencies {
   )
 
   lazy val test: Seq[ModuleID] = Seq(
-    "com.typesafe.play"     %% "play-test"    % "2.8.15"  % "test",
-    "org.pegdown"           %  "pegdown"      % "1.6.0"   % "test",
-    "org.scalatest"         %% "scalatest"    % "3.2.12"  % "test",
-    "org.mockito"           %  "mockito-core" % "4.5.1"   % "test",
-    "com.vladsch.flexmark"  % "flexmark-all"  % "0.62.2"  % "test"
+    "com.typesafe.play"     %% "play-test"                % "2.8.15"  % "test",
+    "org.pegdown"           %  "pegdown"                  % "1.6.0"   % "test",
+    "org.scalatest"         %% "scalatest"                % "3.2.12"  % "test",
+    "org.mockito"           %  "mockito-core"             % "4.5.1"   % "test",
+    "com.vladsch.flexmark"  % "flexmark-all"              % "0.62.2"  % "test",
+    "uk.gov.hmrc"           %% "bootstrap-test-play-28"   % "6.3.0"   % "test"
   )
 }
